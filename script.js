f862f25ded89f194fc821d78f835a0ef3e910d1d
Nikolozi2004 <undeadmaster08@gmail.com>
2024-05-06 16:24:53 +0400

    testing canvas background for mobile

--- script.js
+++ script.js
@@ -189,3 +189,195 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const canvas = document.getElementById('canvas1')
+const c = canvas.getContext('2d')
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+
+c.fillStyle = 'rgb(15, 15, 30)'
+c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillStyle = 'white'
+c.strokeStyle = 'white'
+
+let configs = {
+    numOfParticles: 3500,
+    particleSize: .7,
+    cellSize: 20,
+    curve: 2,
+    zoom: 5,
+    timer: .1,
+    hue: 0,
+    colorRange: 1,
+    gridOverlay: 0
+}
+
+class Particle {
+    constructor(effect) {
+        this.effect = effect
+        this.x = Math.floor(Math.random() * this.effect.width)
+        this.y = Math.floor(Math.random() * this.effect.height)
+        this.speedX
+        this.speedY
+        this.speedModifier = Math.floor(Math.random() * 3 + 1)
+        this.timer = Math.floor(Math.random() * 20 + 1) * configs.timer
+    }
+    draw(context) {
+        context.fillStyle = `hsl(${Math.floor(Math.random() * configs.colorRange * 90 + configs.hue)}, 100%, 50%)`
+        context.fillRect(this.x, this.y, configs.particleSize, configs.particleSize)
+    }
+    drawGrid(context) {
+        c.save()
+        c.strokeStyle = 'white'
+        c.lineWidth = 0.1
+        for (let col = 1; col <= this.effect.cols; col++) {
+
+            c.beginPath()
+            c.moveTo(configs.cellSize * col, 0)
+            c.lineTo(configs.cellSize * col, this.effect.height)
+            c.stroke()
+        }
+        for (let row = 1; row <= this.effect.rows; row++) {
+            c.beginPath()
+            c.moveTo(0, configs.cellSize * row)
+            c.lineTo(this.effect.width, configs.cellSize * row)
+            c.stroke()
+        }
+        c.restore()
+    }
+    update() {
+        this.timer -= 0.01
+        if (configs.gridOverlay) {
+            this.drawGrid()
+        }
+        if (this.timer > 0) {
+            let x = Math.floor(this.x / this.effect.configs.cellSize)
+            let y = Math.floor(this.y / this.effect.configs.cellSize)
+            let index = y * this.effect.cols + x
+            this.angle = this.effect.flowField[index]
+            this.speedX = Math.sin(this.angle)
+            this.speedY = Math.cos(this.angle)
+
+            this.x += this.speedX * this.speedModifier
+            this.y += this.speedY * this.speedModifier
+        } else {
+            this.x = Math.floor(Math.random() * this.effect.width)
+            this.y = Math.floor(Math.random() * this.effect.height)
+            this.timer = Math.floor(Math.random() * 2 + 1) * configs.timer
+        }
+
+    }
+    reset() {
+
+    }
+}
+
+class Effect {
+    constructor(canvas) {
+        this.canvas = canvas
+        this.width = this.canvas.width
+        this.height = this.canvas.height
+        this.particles = []
+        this.rows
+        this.flowField = []
+        this.configs = configs
+        this.init()
+    }
+    init() {
+
+        // create flow field
+        this.rows = Math.floor(this.height / this.configs.cellSize)
+        this.cols = Math.floor(this.width / this.configs.cellSize)
+        this.flowField = []
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                let angle = (Math.cos(x / configs.zoom) + Math.sin(y / configs.zoom)) * configs.curve
+                this.flowField.push(angle)
+            }
+        }
+        // console.log(this.flowField);
+        // create particles
+        this.particles = []
+        for (let i = 0; i < configs.numOfParticles; i++) {
+            this.particles.push(new Particle(this))
+        }
+        // it gets so slow after 3 times of resize :((
+        window.addEventListener('resize', (e) => {
+            this.resize(e.target.innerWidth, e.target.innerHeight)
+        })
+    }
+    resize(width, height) {
+        // this.canvas.width = width
+        // this.canvas.height = height
+        this.width = width
+        this.height = height
+        this.init()
+    }
+    render(context) {
+        this.particles.forEach(particle => {
+            particle.draw(context)
+            particle.update()
+        })
+    }
+}
+
+const effect = new Effect(canvas)
+
+const gui = new dat.GUI();
+gui.add(configs, 'numOfParticles', 100, 4000).onChange(() => effect.init())
+gui.add(configs, 'zoom', .01, 20).onChange(() => effect.init())
+gui.add(configs, 'cellSize', .01, 50).onChange(() => effect.init())
+gui.add(configs, 'curve', .01, 10).onChange(() => effect.init())
+gui.add(configs, 'particleSize', .1, 3)
+gui.add(configs, 'timer', .05, 7)
+gui.add(configs, 'hue', 0, 360)
+gui.add(configs, 'colorRange', 0, 4)
+gui.add(configs, 'gridOverlay', 0, 1, 1)
+
+function animate() {
+    c.fillStyle = 'rgba(0, 0, 15, .03)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    effect.render(c)
+    requestAnimationFrame(animate)
+
+}
+animate()
